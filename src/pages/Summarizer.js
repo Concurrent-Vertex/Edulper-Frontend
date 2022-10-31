1afc4e7fde351c747d473620b74272ae352f5d00
Yashashwini Dixit <yashashwinid@valuefy.com>
2022-10-31 23:16:44 +0530

    summarizer page made

--- src/pages/Summarizer.js
+++ src/pages/Summarizer.js
@@ -1,5 +1,38 @@
-import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-export default function Summarizer() {
-  return <div>Summarizer</div>;
+function Summarizer() {
+  return (
+    <div>
+      <Form
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          textAlign: "center",
+        }}
+      >
+        <Form.Control
+          as="textarea"
+          placeholder="Enter text to summarize"
+          rows={3}
+          style={{ width: "50%", margin: "10px" }}
+        />
+        <Button variant="primary" type="submit" style={{ margin: "10px" }}>
+          Summarize Text
+        </Button>
+        <Form.Control
+          as="textarea"
+          placeholder="Summarized Text"
+          readOnly
+          rows={3}
+          style={{ width: "50%", margin: "10px" }}
+        />
+        <Button onClick={() => alert("Copied")}>Copy Text</Button>
+      </Form>
+    </div>
+  );
 }
+
+export default Summarizer;
