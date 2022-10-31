3f3f7f4e7e59367b2c6273491fa9f5946a12a65e
Yashashwini Dixit <yashashwinid@valuefy.com>
2022-10-31 23:36:36 +0530

    home page and page not found made

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        minHeight: "50vh",
+      }}
+    >
+      <h1>404: Page Not Found</h1>
+    </div>
+  );
 }
